151dd1facecffed3beaa9cb5d18058579b223ee2
adamcarrier <acarrier@marinersmuseum.org>
2014-07-08 14:50:20 -0400

    Initial commit

---
+++ wp-tab-anchors/wp-tab-anchors.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: WP Tab Anchors
+ * Description: This plugin allows you to permalink (i.e., bookmark) to content on a Bootstrap or jQuery UI tab via hash tag links.
+ * Version: 1.0.0
+ * Author: Adam Carrier
+ * Author URI: http://about.me/adamcarrier
+ * License: GPLv2
+ */
+
+// safety first
+if ( ! defined( 'ABSPATH' ) ) die;
+
+wp_enqueue_script('jquery');
+
+function scripts() {
+	wp_enqueue_script( 'wp-tab-anchors', plugin_dir_url( __FILE__ ) . 'wp-tab-anchors.js', false, '1.0.0' );
+	wp_enqueue_script( 'jquery-scrollTo', plugin_dir_url( __FILE__ ) . 'jquery.scrollTo.min.js', array( 'jquery' ), '1.4.11' );
+}
+
+add_action( 'wp_enqueue_scripts', 'scripts' );
+
+?>
